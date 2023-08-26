35c02265b1f07638a012d595571179d4fdd3569a
ASHISH DINESH UPADHYAY <123284935+Imashish-45@users.noreply.github.com>
2023-08-26 23:18:17 +0530

    Add files via upload

---
+++ Dialogflow.py
@@ -0,0 +1,43 @@
+{
+  "responseId": "f231e26d-ec5f-4269-afe4-e193f4f0accc-6318e683",
+  "queryResult": {
+    "queryText": "Ashish, ansh@gmail.com, 9182929123, Mumbai, 23/09/2000",
+    "parameters": {
+      "email": "ansh@gmail.com",
+      "geo-city": "Mumbai",
+      "number": 9182929123,
+      "date-time": "2000-09-23T12:00:00+05:30",
+      "person": {
+        "name": "Ashish"
+      }
+    },
+    "allRequiredParamsPresent": true,
+    "fulfillmentText": "Thank You for the registration. Our team will get in touch with you soon.",
+    "fulfillmentMessages": [
+      {
+        "text": {
+          "text": [
+            "Thank You for the registration. Our team will get in touch with you soon."
+          ]
+        }
+      }
+    ],
+    "intent": {
+      "name": "projects/learntube-gdmn/agent/intents/a6978481-24d1-4ab2-9e7b-412a850ed5c5",
+      "displayName": "Data_gathered",
+      "endInteraction": true
+    },
+    "intentDetectionConfidence": 1,
+    "diagnosticInfo": {
+      "end_conversation": true,
+      "webhook_latency_ms": 872
+    },
+    "languageCode": "en",
+    "sentimentAnalysisResult": {
+      "queryTextSentiment": {}
+    }
+  },
+  "webhookStatus": {
+    "message": "Webhook execution successful"
+  }
+}
